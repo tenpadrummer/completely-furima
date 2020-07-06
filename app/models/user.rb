8ad6670c8358ab203c28bff893409f670d2c46b0
tenpadrummer <4460drums@gmail.com>
2020-07-06 16:28:46 +0900

    edit model/user_spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  #英数字、アンダースコア (_)、プラス (+)、ハイフン (-)、ドット (.) のいずれかを少なくとも1文字以上繰り返す,@マーク, 英小文字、数字、ハイフン、ドットのいずれかを少なくとも1文字以上繰り返す、ドット, 	英小文字を少なくとも1文字以上繰り返す
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  # 半角英数字をそれぞれ1種類以上含む正規表現
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i
+  # 全角ひらがな、全角カタカナ、漢字
+  JAPANESE_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  # 全角カタカナ
+  FULL_WIDTH_REGEX = /\A[ァ-ヶー－]+\z/
+
   validates_presence_of :nickname, :birth_day, :first_name, :last_name, :first_name_reading, :last_name_reading
-  validates :email, uniqueness: { case_sensitive: true }
+  validates :email, uniqueness: { case_sensitive: true }, format: { with: EMAIL_REGEX }
+  validates :password, length: {minimum: 6}, format: { with: PASSWORD_REGEX }
+  validates :first_name, :last_name, format: { with: JAPANESE_REGEX}
+  validates :first_name_reading, :last_name_reading, format: {with: FULL_WIDTH_REGEX }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,10 +4,11 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.name }
     email { Faker::Internet.email }
-    password { 'test' + Faker::Internet.password(min_length: 7, max_length: 20) }
+    password { 'test1234' }
+    password_confirmation { 'test1234' }
     birth_day { Faker::Date.between_except(from: 1.year.ago, to: 1.year.from_now, excepted: Date.today) }
-    first_name { 'Test' }
-    last_name { 'User' }
+    first_name { '田中' }
+    last_name { '花子' }
     first_name_reading { 'テスト' }
     last_name_reading { 'ユーザー' }
   end
