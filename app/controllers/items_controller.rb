039e11454c5d57609218f5c5b28be1e1d10ed8c0
tenpadrummer <4460drums@gmail.com>
2020-07-08 17:50:25 +0900

    edit item_controller for new/create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,25 @@
 class ItemsController < ApplicationController
   def new
-
+    @item = Item.new
   end
 
   def create
-
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      flash[:notice] = '出品に失敗しました'
+      render :new
+    end
   end
+
   private
 
   def item_params
+    params.require(:item).permit(:name, :price, :description, :scheduled_delivery_id, :shipping_fee_status_id, :prefecture_id, :sales_status_id, :category_id, :image
+    ).merge(user_id: current_user.id)
+  end
 
   end
 
