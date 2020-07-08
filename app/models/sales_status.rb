c1f6bf4b8a2dd772a3542f74b24049fb4dccb1a7
tenpadrummer <4460drums@gmail.com>
2020-07-08 18:21:31 +0900

    complete item new/create function

--- app/models/sales_status.rb
+++ app/models/sales_status.rb
@@ -1,11 +1,11 @@
 class SalesStatus < ActiveHash::Base
   self.data = [
-    { id: 0, example: '---' },
-    { id: 1, example: '新品、未使用' },
-    { id: 2, example: '未使用に近い' },
-    { id: 3, example: '目立った傷や汚れなし' },
-    { id: 4, example: 'やや傷や汚れあり' },
-    { id: 5, example: '傷や汚れあり' },
-    { id: 6, example: '全体的に状態が悪い' }
+    { id: 0, condition: '---' },
+    { id: 1, condition: '新品、未使用' },
+    { id: 2, condition: '未使用に近い' },
+    { id: 3, condition: '目立った傷や汚れなし' },
+    { id: 4, condition: 'やや傷や汚れあり' },
+    { id: 5, condition: '傷や汚れあり' },
+    { id: 6, condition: '全体的に状態が悪い' }
   ]
 end
