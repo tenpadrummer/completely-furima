8dc9edb1ff67d980dd5a3485a5405464a61a1d69
tenpadrummer <4460drums@gmail.com>
2020-07-15 00:54:42 +0900

    generate item_purchase controller

---
+++ app/controllers/item_purchases_controller.rb
@@ -0,0 +1,14 @@
+class ItemPurchasesController < ApplicationController
+  def index
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  private
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "items#index"
-
-  resources :items, only: [:show, :new, :create]
+  resources :items, only: [:show, :new, :create] do
+    resources :item_purchases, only: [:index, :new, :create]
+  end
 end
