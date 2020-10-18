e28c7d4a7ce80ec435453e6bfa2b2f68bce51296
tenpadrummer <4460drums@gmail.com>
2020-10-18 15:13:49 +0900

    price,js complete

---
+++ app/javascript/price.js
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const priceInput = document.getElementById("price-input");
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profitDom = document.getElementById("profit");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1).toLocaleString();
+    profitDom.innerHTML = Math.floor(inputValue * 0.9).toLocaleString();
+  })
+});
