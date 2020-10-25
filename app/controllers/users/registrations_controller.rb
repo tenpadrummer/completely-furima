88375fb31a3753b9f47bfea8f3c2aade9d8513b0
tenpadrummer <4460drums@gmail.com>
2020-10-25 12:37:31 +0900

    complete user_info registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,10 +9,34 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    @user = User.new(sign_up_params) #deviseの仕様。configure_sign_up_paramsなのにsign_up_paramsで呼び出せている。
+    unless @user.valid?
+      render :new and return # and returnと記述することで処理を中断
+    end
+    session["devise.regist_data"] = {user: @user.attributes} # attributesメソッドで@userの属性値をハッシュで取得
+    session["devise.regist_data"][:user]["password"] = params[:user][:password] # passwordは@user.attributesに含まないためparamsから別途取得
+    @user_info = @user.build_user_info # @userに紐づく変数をビューで使用するため定義
+    render :new_user_info
+  end
+
+  def create_user_info
+    @user = User.new(session["devise.regist_data"]["user"]) # sessionに入っているuserの情報を元に@userを再定義
+    @user_info = UserInfo.new(user_info_params) # @user_infoを定義
+    @user.build_user_info(@user_info.attributes) # @address_presetと@userを紐づけ
+    unless @user.valid?
+      render :new_user_info and return # and returnと記述することで処理を中断
+    end
+    @user.save #保存
+    session["devise.regist_data"]["user"].clear #sessionを削除
+    sign_in(:user, @user) #ログイン
+  end
+
+  private
+
+  def user_info_params
+    params.require(:user_info).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number)
+  end
 
   # GET /resource/edit
   # def edit
--- app/models/user_info.rb
+++ app/models/user_info.rb
@@ -2,7 +2,7 @@ class UserInfo < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :postal_code, :city, :addresses, :prefecture_id
-  validates :prefecture, numericality: { other_than: 0 }
+  validates :prefecture_id, numericality: { other_than: 0 }
   validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
   validates :phone_number, format: { with: /\A\d{11}\z/ }, length: { maximum: 11 }
 end
