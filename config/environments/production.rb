a9ec2a7480c231bf57590efae81b60d397409240
tenpadrummer <4460drums@gmail.com>
2020-10-25 14:22:14 +0900

    rename

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "furima_clone_kato_production"
+  # config.active_job.queue_name_prefix = "completely_furima_production"
 
   config.action_mailer.perform_caching = false
 
